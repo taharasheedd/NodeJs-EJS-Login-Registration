4da94449f41d6f41b1c04825638aa3b12806a0db
taharasheedd <your_email_address@example.com>
2022-03-17 10:29:47 +0500

    3

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
 const ejs = require('ejs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const encrypt = require('mongoose-encryption');
 const app = express();
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true }, (err) => {
+app.use(session({
+    secret: 'I am a Secret.',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+mongoose.connect("mongodb://localhost:27017/encryptedUserWithBcryptDB", { useNewUrlParser: true }, (err) => {
     if (err) {
         console.log(err);
     } else {
@@ -23,10 +35,12 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-var secret = "IamaSecret"
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
+
+const saltRounds = 10;
+
 
 
 app.listen(3000, (req, res) => {
@@ -46,16 +60,19 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+    const passwordToHashLogin = req.body.password;
     User.findOne({ username: req.body.username }, (err, userFound) => {
         if (err) {
             console.log(err);
         } else {
             if (userFound) {
-                if (userFound.password == req.body.password) {
-                    res.render("secrets")
-                } else {
-                    console.log(err);
-                }
+                bcrypt.compare(passwordToHashLogin, userFound.password, (err, result) => {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        console.log("IDK PW " + err);
+                    }
+                });
             }
 
         }
@@ -64,19 +81,26 @@ app.post("/login", (req, res) => {
 
 
 app.post("/register", (req, res) => {
-
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
-    newUser.save((err) => {
+    passwordToHashRegister = req.body.password;
+    bcrypt.hash(passwordToHashRegister, saltRounds, (err, hashedPassword) => {
         if (err) {
             console.log(err);
         } else {
-            console.log("success");
-            res.render("secrets");
+            const newUser = new User({
+                username: req.body.username,
+                password: hashedPassword
+            });
+            newUser.save((err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("success");
+                    res.render("secrets");
+                }
+            });
         }
     });
+
 });
 
 app.get("/logout", (req, res) => {
